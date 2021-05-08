7f13a4896108b11d5ed03b4d93666998533963d3
trinitanya17 <76651578+trinitanya17@users.noreply.github.com>
2021-05-08 17:53:48 +0530

    Add files via upload
    hi

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+
+fill("white")
+textSize(15)
+text("A T E D A S B A", 700,150)
+fill("lightblue")
+text("Hint: Store The Data !!", 700,170)
+
+
+fill("white")
+textSize(15)
+text("C U T N I F O N", 100,400)
+fill("lightblue")
+text("Hint: Execute The Particular Action !!", 100,420)
+}
